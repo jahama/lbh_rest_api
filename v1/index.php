334d1191b4614868b97f2aca52788fd5eb95eee0
Javi de la Haza <javi.delahaza@gmail.com>
2014-02-28 23:43:22 +0100

    Añadido el codigo para controlar el login del usuario. Despues de validar los datos (usuario, contraseña) el API Key sera enviado en el JSON de respuesta.

--- v1/index.php
+++ v1/index.php
@@ -128,6 +128,48 @@ $app->post('/login', function() use ($app) {
             echoRespnse(200, $response);
         });
 
+/**
+ * User Login
+ * url - /login
+ * method - POST
+ * params - email, password
+ */
+$app->post('/login', function() use ($app) {
+            // check for required params
+            verifyRequiredParams(array('email', 'password'));
+
+            // reading post params
+            $email = $app->request()->post('email');
+            $password = $app->request()->post('password');
+            $response = array();
+
+            $db = new DbHandler();
+            // check for correct email and password
+            if ($db->checkLogin($email, $password)) {
+                // get the user by email
+                $user = $db->getUserByEmail($email);
+
+                if ($user != NULL) {
+                    $response["error"] = false;
+                    $response['name'] = $user['name'];
+                    $response['email'] = $user['email'];
+                    $response['apiKey'] = $user['api_key'];
+                    $response['createdAt'] = $user['created_at'];
+                } else {
+                    // unknown error occurred
+                    $response['error'] = true;
+                    $response['message'] = "An error occurred. Please try again";
+                }
+            } else {
+                // user credentials are wrong
+                $response['error'] = true;
+                $response['message'] = 'Login failed. Incorrect credentials';
+            }
+
+            echoRespnse(200, $response);
+        });
+
+
 /*
  * ------------------------ METHODS WITH AUTHENTICATION ------------------------/*
  * ------------------------   Handling the API calls    ------------------------
